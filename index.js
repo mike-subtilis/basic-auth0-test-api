01485e8bb02a205de1f24794b839a202525f4569
mike-subtilis <mike@subtilis-software.com>
2024-01-03 22:05:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
+const express = require('express');
+const https = require("https");
 const app = express();
-const port = process.env.API_PORT || 3000;
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
+const server = https.createServer(app);
+
+if (process.env.API_PORT) {
+    server.listen(process.env.API_PORT, () => {
+        console.log(`server is running on port ${process.env.API_PORT}...`);
+    });
+} else {
+    server.listen(() => {
+        console.log(`server is running on default port which may be ${server.address().port}...`);
+    });
+}
+
