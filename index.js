2bfc264469c39c8c4e96da863081631b21a314b6
mike-subtilis <mike@subtilis-software.com>
2024-01-04 13:13:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-
+  
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
+});
+
+app.get('/', (req, res) => {
+    res.send('Blank page')
 });
 
 const port = process.env.API_PORT || 3001;
