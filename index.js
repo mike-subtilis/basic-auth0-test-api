211009b76d931968f35b89c34dc9b1001ea9fa61
mike-subtilis <mike@subtilis-software.com>
2024-01-04 11:54:36 -0700

    removing https for a bit

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const https = require("https");
 const app = express();
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
 });
 
-const server = https.createServer(app);
-
-if (process.env.API_PORT) {
-    server.listen(process.env.API_PORT, () => {
-        console.log(`server is running on port ${process.env.API_PORT}...`);
-    });
-} else {
-    server.listen(() => {
-        console.log(`server is running on default port which may be ${server.address().port}...`);
-    });
-}
+const port = process.env.API_PORT || 3001;
+app.listen(port, () => {
+    console.log(`server is running on port ${port}...`);
+});
 
